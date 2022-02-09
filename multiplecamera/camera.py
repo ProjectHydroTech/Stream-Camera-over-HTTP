f0b3a75b86d527a91ff5ffa0cf2a3ba941c5b35d
HydroTech <92703672+ProjectHydroTech@users.noreply.github.com>
2022-02-09 15:54:38 +0800

    Update camera.py

--- multiplecamera/camera.py
+++ multiplecamera/camera.py
@@ -2,15 +2,7 @@ from importlib import import_module
 import os
 from flask import Flask, render_template, Response
 
-# import camera driver
-'''
-if os.environ.get('CAMERA'):
-    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-else:
-    from camera import Camera
-'''
-#
-from camera_opencv import Camera
+from camera_opencv import Camera #local import
 
 
 app = Flask(__name__)
